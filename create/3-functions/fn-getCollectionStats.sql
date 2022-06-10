24edd26cf824bb3137f0c272b468950f14b93ddb
richardbadman <richard.a.g@hotmail.co.uk>
2022-06-10 20:04:51 +0100

    [feat] Function for collection stats

---
+++ create/3-functions/fn-getCollectionStats.sql
@@ -0,0 +1,70 @@
+-------------------------------------------------------------------------------
+-- Created      10-06-2022
+-- Purpose      Returns top collections which are paginated and sortable
+-- Copyright © 2022, Zilkroad, All Rights Reserved
+-------------------------------------------------------------------------------
+-- Modification History
+--
+-- 10-06-2022 Rich - Inital creation
+-------------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION fn_getCollectionStats
+(
+	_nonfungible_address varchar
+) 
+RETURNS TABLE
+(
+    listed_tokens bigint,
+    volume numeric(15, 2)
+)
+AS 
+$BODY$
+BEGIN
+
+    RETURN QUERY
+    SELECT 
+        listing.count as listed_tokens,
+        volume.volume as volume
+        FROM 
+        (
+            SELECT count(listing_id) as count
+            FROM tbl_static_listing tsl
+
+            INNER JOIN tbl_nonfungible_token tnft
+                ON tsl.extract_nft_id = tnft.extract_nft_id
+
+            INNER JOIN tbl_nonfungible tnf
+                ON tnft.nonfungible_id = tnf.nonfungible_id
+
+            WHERE nonfungible_address = _nonfungible_address
+            AND tsl.listing_id NOT IN (
+                SELECT listing_id
+                FROM tbl_static_delisting
+                UNION ALL
+                SELECT listing_id
+                FROM tbl_static_sale
+            )
+        ) listing ,
+        (
+            SELECT COALESCE(SUM(final_sale_after_taxes_usd), 0) as volume
+
+            FROM tbl_static_sale tss
+
+            INNER JOIN tbl_static_listing tsl
+                ON tss.listing_id = tsl.listing_id
+
+            INNER JOIN tbl_nonfungible_token tnft
+                ON tsl.extract_nft_id = tnft.extract_nft_id
+
+            INNER JOIN tbl_nonfungible tnf
+                ON tnft.nonfungible_id = tnf.nonfungible_id
+
+            WHERE nonfungible_address = _nonfungible_address
+            AND tss.listing_id NOT IN (
+                SELECT listing_id
+                FROM tbl_static_delisting
+            )
+        ) volume;
+
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE SECURITY DEFINER COST 100;
