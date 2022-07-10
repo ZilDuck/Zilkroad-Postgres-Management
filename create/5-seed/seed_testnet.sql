98666f777554f8830800deb5b12dc02c78d7a138
Elliott Green <eg145@brighton.ac.uk>
2022-07-10 23:19:48 +0100

    [chore] fix fungible data and add more verified seed data

--- create/5-seed/seed_testnet.sql
+++ create/5-seed/seed_testnet.sql
@@ -10,7 +10,7 @@ values
 ('0x607d0ba69c3680ed07262e5d80be98c401fd612c','Testnet Governance Zilliqa','GZIL',15),
 ('0x56906c825a6df74032ea40b4ff882b5970f3d0a8','Testnet StraitsX Singapore Dollar','XSGD',6),
 ('0x9fe903bf7c48d6bf4bcc7c53e77d3d2a2ebbf5ae','Testnet Wrapped Zilliqa Bitcoin','zWBTC',8),
-('0x2e8e7d992c8dc04f89c4e5e798b2fa8345885b18','Testnet Wrapped Zilliqa Ethereum','zWETH',18),
+('0x2e8e7d992c8dc04f89c4e5e798b2fa8345885b18','Testnet Wrapped Zilliqa Ethereum','zETH',18),
 ('0x9fe903bf7c48d6bf4bcc7c53e77d3d2a2ebbf5ae','Testnet Wrapped Zilliqa Tether Dollar','zUSDT', 6),
 ('0x30832ed39781b7840485f79fb971bb67a595a2b2','Testnet Duck Duck','DUCK', 2)
 ON CONFLICT DO NOTHING;
