e2060e44167e0722a51e240bb3e387b74e416cbe
Richard Goodman <richardg@brandwatch.com>
2022-09-28 16:46:26 +0100

    Update fn-getPaginatedListingForUser.sql

--- create/3-functions/fn-getPaginatedListingForUser.sql
+++ create/3-functions/fn-getPaginatedListingForUser.sql
@@ -7,54 +7,75 @@
 --
 -- 17-01-2022  Nines Inital creation.
 -- 16-07-2022  Rich Fix exclusions
+-- 28-09-2022  Rich Add extra information
 -------------------------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION fn_getPaginatedListingForUser
 (
-    _listing_user_address varchar(42),
-    _limit_rows numeric,
-    _offset_rows numeric
+  _listing_user_address varchar(42),
+  _limit_rows numeric,
+  _offset_rows numeric
 ) 
 returns TABLE
-        (
-            listing_user_address         varchar,
-            static_order_id              int8,
-            token_id                     numeric,
-            nonfungible_address          varchar,
-            listing_transaction_hash     varchar,
-            fungible_address             varchar,
-            listing_fungible_token_price numeric,
-            listing_block                numeric,
-            listing_unixtime             int8
-        )
+(
+  listing_user_address         varchar,
+  static_order_id              int8,
+  token_id                     numeric,
+  nonfungible_address          varchar,
+  listing_transaction_hash     varchar,
+  fungible_address             varchar,
+  listing_fungible_token_price numeric,
+  fungible_symbol              varchar,
+  decimals                     numeric,
+  listing_block                numeric,
+  listing_unixtime             int8,
+  verified                     boolean
+)
 AS 
 $BODY$
 BEGIN
 
-    RETURN QUERY
-        select tsl.listing_user_address,
-               tsl.static_order_id,
-               tnt.token_id,
-               tnf.nonfungible_address,
-               tsl.listing_transaction_hash,
-               tf.fungible_address,
-               tsl.listing_fungible_token_price,
-               tsl.listing_block,
-               tsl.listing_unixtime
-        from tbl_static_listing tsl
-                 left join tbl_nonfungible_token tnt
-                           on tsl.extract_nft_id = tnt.extract_nft_id
-                 left join tbl_fungible tf
-                           on tsl.fungible_id = tf.fungible_id
-                 left join tbl_nonfungible tnf
-                           on tnf.nonfungible_id = tnt.nonfungible_id
-                 left join tbl_exclude_contract ec
-                           on ec.nonfungible_id = tf.fungible_id
-        where listing_id not in (select listing_id from tbl_static_delisting union all select listing_id from tbl_static_sale)
-          AND ec.exclude_id is null
-          AND tsl.listing_user_address = _listing_user_address
-        LIMIT _limit_rows OFFSET _offset_rows;
-       
+  RETURN QUERY
+  SELECT tsl.listing_user_address,
+         tsl.static_order_id,
+         tnt.token_id,
+         tnf.nonfungible_address,
+         tsl.listing_transaction_hash,
+         tf.fungible_address,
+         tsl.listing_fungible_token_price,
+         tf.fungible_symbol,
+         tf.decimals,
+         tsl.listing_block,
+         tsl.listing_unixtime,
+         CASE WHEN tvc.verified_id IS NULL THEN False ELSE True END AS verified
+         
+         FROM tbl_static_listing tsl
+         
+         LEFT JOIN tbl_nonfungible_token tnt
+            ON tsl.extract_nft_id = tnt.extract_nft_id
+         
+         LEFT JOIN tbl_fungible tf
+            ON tsl.fungible_id = tf.fungible_id
+         
+         LEFT JOIN tbl_nonfungible tnf
+            ON tnf.nonfungible_id = tnt.nonfungible_id
+         
+         LEFT JOIN tbl_exclude_contract ec
+            ON ec.nonfungible_id = tf.fungible_id
+         
+         LEFT JOIN tbl_verified_contract tvc
+            ON tnf.nonfungible_id = tvc.nonfungible_id
+         
+         WHERE listing_id NOT IN (
+            SELECT listing_id FROM tbl_static_delisting 
+            UNION ALL 
+            SELECT listing_id FROM tbl_static_sale
+         )
+         AND ec.exclude_id IS NULL
+         AND tsl.listing_user_address = _listing_user_address
+         
+         LIMIT _limit_rows OFFSET _offset_rows;
+
 END;
 $BODY$
 LANGUAGE plpgsql VOLATILE SECURITY DEFINER COST 100;
