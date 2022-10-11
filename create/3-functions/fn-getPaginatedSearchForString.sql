ed593cb49e2d642107ad88a93b100510f2db0ae2
Richard Goodman <richardagoodman@icloud.com>
2022-10-11 20:41:33 +0100

    Update function for search
    This adds the search term vector querying into the mix
    I've also lessened the similarity as we don't have much data atm, and it is the only way atm in built-in postgres to provide "spelling mistakes"

--- create/3-functions/fn-getPaginatedSearchForString.sql
+++ create/3-functions/fn-getPaginatedSearchForString.sql
@@ -8,10 +8,12 @@
 -- 16-03-2022 Nines - Inital creation.
 -- 09-04-2022 Nines - Add excluded and verified logic, don't remove brackets in where clause.
 -- 22-07-2022 Nines - Resolve bug with freetext of 0x address and add sorting
+-- 11-10-2022 Rich  - Added search term vectors into query
 -------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION fn_getPaginatedSearchForString
 (
     _user_search varchar,
+    _search_term varchar,
     _limit_rows numeric,
 	_offset_rows numeric
 ) 
@@ -39,17 +41,22 @@ RETURN QUERY
     on ex.nonfungible_id = tnt.nonfungible_id
     left join tbl_verified_contract tvc   
     on tvc.nonfungible_id = tnf.nonfungible_id
-    WHERE SIMILARITY(tnf.nonfungible_name, _user_search) > 0.5
-    OR SIMILARITY(tnf.nonfungible_symbol, _user_search) > 0.5
-    OR tnf.nonfungible_address like concat(_user_search, '%') 
+    WHERE (
+        tnf.simple_tsvector @@ to_tsquery('simple', _search_term)
+        OR SIMILARITY(tnf.nonfungible_name, _user_search) > 0.1
+        OR SIMILARITY(tnf.nonfungible_symbol, _user_search) > 0.1
+        OR tnf.nonfungible_address like concat(_user_search, '%') 
+    )
     AND ex.exclude_id is null
     GROUP BY 
         tnf.nonfungible_address, 
         tnf.nonfungible_symbol, 
         tnf.nonfungible_name,
-        tvc.verified_id
+        tvc.verified_id,
+        ts_rank(tnf.simple_tsvector, to_tsquery('english', _search_term))
     ORDER BY 
         tvc.verified_id,
+        ts_rank(tnf.simple_tsvector, to_tsquery('english', _search_term)) desc,
         tnf.nonfungible_name
     LIMIT _limit_rows
     OFFSET _offset_rows;
