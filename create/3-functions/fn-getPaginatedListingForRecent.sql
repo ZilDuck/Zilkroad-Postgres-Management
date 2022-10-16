e0c3104a0dda27a693d82a72963f46879a5a9f51
Richard Goodman <richardagoodman@icloud.com>
2022-10-16 22:50:12 +0100

    Update fn-getPaginatedListingForRecent.sql

--- create/3-functions/fn-getPaginatedListingForRecent.sql
+++ create/3-functions/fn-getPaginatedListingForRecent.sql
@@ -6,11 +6,12 @@
 -- Modification History
 --
 -- 17-01-2022  Nines Inital creation.
+-- 16-10-2022  Rich add verification
 -------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION fn_getPaginatedListingForRecent
 (
-	_limit_rows numeric,
-	_offset_rows numeric
+  _limit_rows numeric,
+  _offset_rows numeric
 ) 
 returns TABLE 
 (
@@ -24,60 +25,65 @@ returns TABLE
     fungible_name varchar,
     fungible_symbol varchar,
     decimals numeric,
-    fungible_address varchar
+    fungible_address varchar,
+    verified boolean
 ) 
 AS 
 $BODY$
 BEGIN
 
 RETURN QUERY
-	SELECT
-		tsl.static_order_id,
-		tnt.token_id,
-		tnf.nonfungible_address,
-		tsl.listing_fungible_token_price,
-		tsl.listing_block,
-		tsl.listing_unixtime,
-		tsl.listing_user_address,
-		tf.fungible_name,
-		tf.fungible_symbol,
-		tf.decimals,
-		tf.fungible_address
+  SELECT
+    tsl.static_order_id,
+    tnt.token_id,
+    tnf.nonfungible_address,
+    tsl.listing_fungible_token_price,
+    tsl.listing_block,
+    tsl.listing_unixtime,
+    tsl.listing_user_address,
+    tf.fungible_name,
+    tf.fungible_symbol,
+    tf.decimals,
+    tf.fungible_address,
+    CASE WHEN tvc.verified_id IS NULL THEN False ELSE True END AS verified
 
-	FROM tbl_static_listing tsl
+  FROM tbl_static_listing tsl
 
-	LEFT JOIN tbl_static_sale tss
-		ON tsl.listing_id = tss.listing_id
+  LEFT JOIN tbl_static_sale tss
+    ON tsl.listing_id = tss.listing_id
 
-	LEFT JOIN tbl_static_delisting tsd 
-		ON tsl.listing_id = tsd.listing_id
+  LEFT JOIN tbl_static_delisting tsd 
+    ON tsl.listing_id = tsd.listing_id
 
-	LEFT JOIN tbl_nonfungible_token tnt
-		ON tsl.extract_nft_id = tnt.extract_nft_id
+  LEFT JOIN tbl_nonfungible_token tnt
+    ON tsl.extract_nft_id = tnt.extract_nft_id
 
-	LEFT JOIN tbl_nonfungible tnf
-		ON tnt.nonfungible_id = tnf.nonfungible_id
+  LEFT JOIN tbl_nonfungible tnf
+    ON tnt.nonfungible_id = tnf.nonfungible_id
 
-	LEFT JOIN tbl_fungible tf
-		ON tsl.fungible_id = tf.fungible_id
+  LEFT JOIN tbl_fungible tf
+    ON tsl.fungible_id = tf.fungible_id
 
-	LEFT JOIN tbl_exclude_contract tec
-		ON tnt.nonfungible_id = tec.nonfungible_id	
+  LEFT JOIN tbl_exclude_contract tec
+    ON tnt.nonfungible_id = tec.nonfungible_id  
+
+  LEFT JOIN tbl_verified_contract tvc
+    ON tnf.nonfungible_id = tvc.nonfungible_id
 
 
-	WHERE tsl.listing_id NOT IN (
-		SELECT listing_id
-		FROM tbl_static_delisting
-		UNION ALL
-		SELECT listing_id
-		FROM tbl_static_sale
-	)
-	AND tec.exclude_id IS NULL
+  WHERE tsl.listing_id NOT IN (
+    SELECT listing_id
+    FROM tbl_static_delisting
+    UNION ALL
+    SELECT listing_id
+    FROM tbl_static_sale
+  )
+  AND tec.exclude_id IS NULL
 
-	ORDER BY tsl.static_order_id
+  ORDER BY tsl.static_order_id
 
-	LIMIT _limit_rows
-	OFFSET _offset_rows;
+  LIMIT _limit_rows
+  OFFSET _offset_rows;
 
 END;
 $BODY$
